b41845006cdbb142f33ce76f3097c41cb120685c
yunius1 <50488305+yunius1@users.noreply.github.com>
2021-07-31 22:36:27 +0700

    new

--- app/Views/home/dashboard.php
+++ app/Views/home/dashboard.php
@@ -133,7 +133,7 @@
     	</div>
     </section>
 
-    <section class="ftco-section">
+    <section class="ftco-section page-scroll" id="saran">
 			<div class="container">
 				<div class="row justify-content-center mb-5 pb-2">
           <div class="col-md-8 text-center heading-section ftco-animate">
