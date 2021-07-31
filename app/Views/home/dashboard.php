6a1c68d2dedd3a45c4e705fd2a70f8d0a38cb012
HaFirs <50348831+HaFirs@users.noreply.github.com>
2021-08-01 00:07:37 +0700

    Update dashboard.php

--- app/Views/home/dashboard.php
+++ app/Views/home/dashboard.php
@@ -41,8 +41,8 @@
   <section id="topbar" class="d-flex align-items-center">
     <div class="container d-flex justify-content-center justify-content-md-between">
       <div class="contact-info d-flex align-items-center">
-        <i class="bi bi-envelope d-flex align-items-center"><a href="mailto:contact@example.com">kot-palangkaraya@atrbpn.go.id</a></i>
-        <i class="bi bi-phone d-flex align-items-center ms-4"><span>(0536) 3220057</span></i>
+        <i class="bi bi-envelope d-flex align-items-center"><a href="mailto:contact@example.com">contact@example.com</a></i>
+        <i class="bi bi-phone d-flex align-items-center ms-4"><span>+1 5589 55488 55</span></i>
       </div>
       <div class="social-links d-none d-md-flex align-items-center">
         <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
@@ -57,18 +57,35 @@
   <header id="header" class="d-flex align-items-center">
     <div class="container d-flex align-items-center justify-content-between">
 
+      <a href="index.html" class="logo"><img src="../images/logo.png" alt=""></a>
       <h1 class="logo"><a href="index.html">BPN Kota Palangka Raya</a></h1>
       <!-- Uncomment below if you prefer to use an image logo -->
-      <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt=""></a>-->
 
       <nav id="navbar" class="navbar">
         <ul>
           <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
-          <li><a class="nav-link scrollto" href="#about">Tentang BPN</a></li>
-          <li><a class="nav-link scrollto" href="#services">Layanan</a></li>
-          <li><a class="nav-link scrollto " href="#portfolio">Saran dan Masukan</a></li>
-        
-          <li><a class="nav-link scrollto" href="#contact">Login</a></li>
+          <li><a class="nav-link scrollto" href="#about">About</a></li>
+          <li><a class="nav-link scrollto" href="#services">Services</a></li>
+          <li><a class="nav-link scrollto " href="#portfolio">Portfolio</a></li>
+          <li><a class="nav-link scrollto" href="#team">Team</a></li>
+          <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="#">Drop Down 1</a></li>
+              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="#">Deep Drop Down 1</a></li>
+                  <li><a href="#">Deep Drop Down 2</a></li>
+                  <li><a href="#">Deep Drop Down 3</a></li>
+                  <li><a href="#">Deep Drop Down 4</a></li>
+                  <li><a href="#">Deep Drop Down 5</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Drop Down 2</a></li>
+              <li><a href="#">Drop Down 3</a></li>
+              <li><a href="#">Drop Down 4</a></li>
+            </ul>
+          </li>
+          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
       </nav><!-- .navbar -->
@@ -79,11 +96,12 @@
   <!-- ======= Hero Section ======= -->
   <section id="hero" class="d-flex align-items-center">
     <div class="container" data-aos="zoom-out" data-aos-delay="100">
-      <h1>Selamat Datang </h1>
-      <h2 class="text-dark"><span>ATR / BPN</span> Kantah Kota Palangka Raya</h2>
+      <h1 class="text-light shadow-sm">Selamat Datang <span class="shadow-sm">ATR/BPN</span></h1>
+      <h2 class="text-light shadow-sm">We are team of talented designers making websites with Bootstrap</h2>
       <div class="d-flex">
-        <a href="#about" class="btn-get-started scrollto">Layanan Kami</a>
-          </div>
+        <a href="#about" class="btn-get-started scrollto">Get Started</a>
+        <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="glightbox btn-watch-video"><i class="bi bi-play-circle"></i><span class="text-light shadow-sm">Watch Video</span></a>
+      </div>
     </div>
   </section><!-- End Hero -->
 
@@ -138,7 +156,7 @@
         <div class="section-title">
           <h2>About</h2>
           <h3>Find Out More <span>About Us</span></h3>
-          <p>Ut possimus qui ut temporibus culpa velit eveniet modi omnis est adipisci expedita at voluptas atque vitae autem.</p>
+          <p>Kementrian Agraria dan Tata Ruang / Badan Pertahanan Nasional Kantor Pertahanan Kota Palangkaraya</p>
         </div>
 
         <div class="row">
