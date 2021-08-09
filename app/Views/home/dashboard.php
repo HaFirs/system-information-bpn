f6e287ffcdd92c77ea05b2f5d77ad9ba5bba9037
den2209 <dendianggriandi@gmail.com>
2021-08-10 02:06:45 +0700

    new

--- app/Views/home/dashboard.php
+++ app/Views/home/dashboard.php
@@ -172,7 +172,8 @@
         </div>
 
       </div>
-    </section><!-- End About Section -->
+    </section>
+    <!-- End About Section -->
 
     <!-- ======= Services Section ======= -->
     <section id="services" class="services">
@@ -629,6 +630,23 @@
       </div>
     </section><!-- End Services Section -->
 
+    <!-- ======= penilain Section ======= -->
+    <section id="penilaian" class="about section-bg">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Penilaian</h2>
+          <h3>Berikan Penilaian Anda Terhadap Pelayanan Kami</h3><br>
+          <a href="<?php echo base_url('dashboard/penilaian'); ?>">
+            <h2><span>Beri Nilai</span></h2>
+          </a>
+        </div>
+        <h3 style="text-align: center;">Grafik Penilain :</h3>
+      </div>
+    </section>
+    <!-- End penilain Section -->
+
+
     <!-- ======= Contact Section ======= -->
     <section id="contact" class="contact">
       <div class="container" data-aos="fade-up">
@@ -649,7 +667,7 @@
             </div>
           </div>
 
-          <div class="col-lg-3 col-md-6">
+          <div class="col-lg-6">
             <div class="info-box  mb-4">
               <i class="bx bx-phone-call"></i>
               <i class="bx bx-envelope"></i>
@@ -659,27 +677,11 @@
               <p>kot-palangkaraya@atrbpn.go.id</p>
             </div>
           </div>
-
-          <div class="col-lg-3 col-md-6">
-            <div class="info-box">
-              <div class="section-title">
-                <p>Berikan Penilaian Anda Terhadap Pelayanan Kami : </p>
-                <br>
-                <a href="<?php echo base_url('dashboard/penilaian'); ?>" class="btn-get-started scrollto">
-                  <h2>Beri Penilaian</h2>
-                </a>
-              </div>
-            </div>
-          </div>
-
-
-
-
         </div>
 
         <div class="row" data-aos="fade-up" data-aos-delay="100">
 
-          <div class="col-lg-6 ">
+          <div class="col-lg-12 ">
             <iframe class="mb-4 mb-lg-0" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d31895.008457723237!2d113.8657215!3d-2.2005362!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dfcb25a7a2fc55b%3A0x3f8d35651cd5d39f!2sBadan%20Pertanahan%20Nasional!5e0!3m2!1sid!2sid!4v1628358276457!5m2!1sid!2sid" frameborder="0" style="border:0; width: 100%; height: 384px;" allowfullscreen></iframe>
           </div>
         </div>
@@ -691,69 +693,12 @@
 
   <!-- ======= Footer ======= -->
   <footer id="footer">
-    <div class="footer-top">
-      <div class="container">
-        <div class="row">
-
-          <div class="col-lg-3 col-md-6 footer-contact">
-            <h3>BizLand<span>.</span></h3>
-            <p>
-              A108 Adam Street <br>
-              New York, NY 535022<br>
-              United States <br><br>
-              <strong>Phone:</strong> +1 5589 55488 55<br>
-              <strong>Email:</strong> info@example.com<br>
-            </p>
-          </div>
-
-          <div class="col-lg-3 col-md-6 footer-links">
-            <h4>Useful Links</h4>
-            <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Pelayanan</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
-            </ul>
-          </div>
-
-          <div class="col-lg-3 col-md-6 footer-links">
-            <h4>Persyaratan</h4>
-            <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
-            </ul>
-          </div>
-
-          <div class="col-lg-3 col-md-6 footer-links">
-            <h4>Our Social Networks</h4>
-            <p>Cras fermentum odio eu feugiat lide par naso tierra videa magna derita valies</p>
-            <div class="social-links mt-3">
-              <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-              <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-              <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-              <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-              <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
-            </div>
-          </div>
-
-        </div>
-      </div>
-    </div>
-
-    <div class="container py-4">
+    <div class="container py-4" style="text-align: center;">
       <div class="copyright">
-        &copy; Copyright <strong><span>BizLand</span></strong>. All Rights Reserved
+        &copy; Copyright <strong><span>PENTHAGOUS</span></strong>. All Rights Reserved
       </div>
-      <div class="credits">
-        <!-- All the links in the footer should remain intact. -->
-        <!-- You can delete the links only if you purchased the pro version. -->
-        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/bizland-bootstrap-business-template/ -->
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      <div class="" style="text-align: right;">
+        Designed by PENTHAGOUS
       </div>
     </div>
   </footer><!-- End Footer -->
